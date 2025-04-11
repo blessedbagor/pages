3a3a9743f4c809df31b251f275448e4b6c3b6153
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-04-11 20:02:16 +0800

    Remove platform pages and components

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <div className="flex-center min-h-screen w-full">{children}</div>
-    )
-  }
